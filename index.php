3a517a567a105e8720826d18a670c1e4b333fb6a
Tomáš Sidorják <tominko99@gmail.com>
2018-07-24 20:49:14 +0200

    dokončený login panel
    ss

--- index.php
+++ index.php
@@ -31,8 +31,28 @@ if (isset($_POST['username'], $_POST['password'])){
     if (empty($user) or empty($pass)){
         $error = "Vsetky policka je potrebne vyplnit";
     }
-}
+}else {
+			$query = $pdo->prepare("SELECT * FROM users WHERE user_name = ? AND user_password = ?");
 
+			$query->bindValue(1, $username);
+			$query->bindValue(2, $password);
+
+			$query->execute();
+
+			$num = $query->rowCount();
+
+			if ($num == 1) {
+				$_SESSION['logged_in'] = true;
+				header("Location: index.php");
+				exit();
+			} else {
+				$error = "Nesprávne meno/heslo alebo nemáte status administrátora";
+			}
+
+
+
+		}
+	}
 ?>
 
 
